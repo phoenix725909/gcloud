258a27471fecf41a0ff880d14138fd4b9c691098
phoenix725909 <phoenix725909@gmail.com>
2024-07-29 13:51:34 -0400

    push code

---
+++ automation/modules/server/main.tf
@@ -0,0 +1,40 @@
+resource "google_compute_instance" "default" {
+  name         		= var.friendly_name
+  machine_type 		= var.machine.type
+  zone         		= var.zone
+  tags 		   		= var.tags
+  hostname     		=join(".", ["${var.friendly_name}", "${var.domain}"])
+  description  		=var.description
+  desired_status 	= var.machine_state
+  labels			= {
+	environment		= var.environment
+	application		= var.application
+	component		= var.component
+	}
+
+
+  boot_disk {
+	device_name		="${var.friendly_name}-boot
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
---
+++ automation/modules/server/variables.tf
@@ -0,0 +1,58 @@
+variable "component" {
+	type = string
+	}
+
+variable "application" {
+	type = string
+	}
+
+variable "friendly_name" {
+	type = string
+	validation {
+	condition = length(va.friendly_name) < 16
+	error_message = "The hostname must be 15 characters or less"
+	}
+	}
+
+variable "machine_type" {
+	type = string
+	default = "n1-standard-2"
+	}
+
+variable "zone" {
+	type = string
+	}
+
+variable "tags" {
+	type = list(string)
+	default []
+	}
+
+variable "environment" {
+	type = list(string)
+	
+	validation{
+	condition= can(contains(["prod", "dr", "dev", "test"], var.environment))
+	error_message = "You may use: prod, dr, dev, test."
+	}
+	}
+	
+variable "description" {
+	type = string
+	}
+
+variable "machine_state" {
+	type = string
+	default = "RUNNING"
+	}
+
+variable "domain" {
+	type = string
+	default = "dragon"
+	}
+
+variable "application" {
+	type = string
+	}
+
+	
